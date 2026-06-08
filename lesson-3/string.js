48e9c599de3b2c8f3c243ad3d205143a95284c61
shiivankiit <shivankit7557@gmail.com>
2026-06-08 23:22:00 +0530

    Basic-string

---
+++ lesson-3/string.js
@@ -0,0 +1,23 @@
+//A string represent a text.
+console.log('hello');
+//combination of string is called concatination.
+console.log('hello' + 'sam');
+console.log(typeof('hello'));
+
+//Revision.
+const name='Andrew';
+const repoCount=50;
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename=new String('Yadav-ji');
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+
+//To find the length of string we use (.length)
+//Converting a string to uppercase we use (.toUpperCase())
+//to find the position of a char we use charAt(3).
+
+//There's a lot more function to work with a string.
+
+

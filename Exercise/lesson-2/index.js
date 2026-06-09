f5e84acaf88d9f3ab9989ab19f812f2f5ffc3a31
shiivankiit <shivankit7557@gmail.com>
2026-06-09 22:54:28 +0530

    Basics-Questions

---
+++ Exercise/lesson-2/index.js
@@ -0,0 +1,15 @@
+// Lesson-1 exercise done in console.
+
+//2a:-Visit the assignment.
+console.log(10+8+5);
+//2b:-
+console.log((10 + 3 * 8 + 5) / 3);
+//2c
+console.log(18.50+(2*7.50));
+//2d
+console.log(18.50+(2*7.50))*0.10
+//2e
+console.log(18.50+(2*7.50))*0.20;
+
+
+//All other questions done in console all of them are easy and basic math questions.

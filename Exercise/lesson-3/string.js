41decfffa3dcb805cf228fd7f2cbef522a7cf854
shiivankiit <shivankit7557@gmail.com>
2026-06-15 23:50:56 +0530

    String in Java-Script.

---
+++ Exercise/lesson-3/string.js
@@ -0,0 +1,16 @@
+// String in JavaScript.
+
+//3a:-
+let str='My name is:-'
+console.log(str);
+//3b:-
+const name='Ronaldo';
+//3c:-
+console.log(str+name);
+//3d:-
+let coffe=5;
+let bag=3;
+let sum=coffe+bag;
+console.log(`Total cost is ${sum}$`);
+//3f
+console.log(alert(`Total cost is ${sum}$`));

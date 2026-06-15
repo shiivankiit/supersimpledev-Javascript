8cc911cfaa931a959822d67b64a34b2f79d8ae00
shiivankiit <shivankit7557@gmail.com>
2026-06-15 23:50:32 +0530

    More question added

--- Exercise/lesson-2/index.js
+++ Exercise/lesson-2/index.js
@@ -10,6 +10,11 @@ console.log(18.50+(2*7.50));
 console.log(18.50+(2*7.50))*0.10
 //2e
 console.log(18.50+(2*7.50))*0.20;
+//2f-2g
+console.log(47.93+4.99);
+//2h
+console.log(52.92+5.29);
 
 
 //All other questions done in console all of them are easy and basic math questions.
+//Using Math.floor and Math.ceil we will round of the no.
